5e2b779c711c2e9b4c332196a69b86bfd0d54056
frame-570 <framef570@gmail.com>
2026-08-12 15:36:15 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-export const SUPABASE_URL = "https://orfurfhwcbzihacgwlva.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_sEDz9yhZx8SuwypjSwmP1w_OvTo8HQS";
+export const SUPABASE_URL = "https://zclefxrshkeyozprnpfb.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_GLwQqBTzFDn17Dncuj2XBA_irN8TWB4";
 export const FUNCTIONS_URL = `${SUPABASE_URL}/functions/v1`;
